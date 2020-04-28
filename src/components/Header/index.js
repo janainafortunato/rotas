bde3007142da4ccb34d2392550e74412e5f942fa
janainafortunato <janaina.projetos@live.com>
2020-04-28 10:48:57 -0300

    estudos de react.js, rotas

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'; 
+
+class Header extends Component{
+  
+  render(){
+    return(
+        <div>
+            Header meu projeto<br />
+            <Link to="/sobre">Home</Link>
+            <hr/>
+        </div>
+
+    );
+  }
+}
+
+export default Header;
